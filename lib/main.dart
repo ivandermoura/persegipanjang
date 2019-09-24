d9c70c9125e7896a15e7421816cf38fa2111155a
ivandermoura <ivanderlp1@gmail.com>
2019-09-24 10:02:09 +0700

    komit pertama

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+//import 'hp.dart';
+void main() => runApp(Luassegiempat());
+
+class Luassegiempat extends StatefulWidget {
+
+  
+  @override
+  _LuassegiempatState createState() => _LuassegiempatState();
+}
+
+class _LuassegiempatState extends State<Luassegiempat> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Luas Segi Empat",
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home:  new Homepage(),
+      
+    );
+  }
+}

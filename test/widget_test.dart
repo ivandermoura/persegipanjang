3fbb30caf94b2f98808ab97ebd9ebd3745f43a42
ivandermoura <ivanderlp1@gmail.com>
2019-09-27 10:35:57 +0700

    update ketiga

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:luas_segiempat/home_page.dart';
 import 'package:luas_segiempat/main.dart';
+import 'package:luas_segiempat/home_page.dart';
 void main(){
 
   test("Panjang persegi return error string",() {
@@ -15,19 +15,14 @@ void main(){
   });
    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Homepage());
+    await tester.pumpWidget(Luassegiempat());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
+    
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+   
   });
 
 }

c0763298494752b0406f38a75f3de81aeed49b73
ivandermoura <ivanderlp1@gmail.com>
2019-11-06 12:32:09 +0700

    update test-ketiga

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,10 @@ import 'package:luas_segiempat/main.dart';
 import 'package:luas_segiempat/home_page.dart';
 
 void main(){
-
+  Widget maketestablewidget({Widget child}){
+    return MaterialApp(home: child,);
+  }
+ 
   test("Panjang persegi return error string",() {
     var result = Panjangvalidator.validate('');
     expect(result, 'kolom panjang tidak boleh kosong');
@@ -14,16 +17,40 @@ void main(){
     var result = Panjangvalidator.validate('string');
     expect(result, null);
   });
-   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+   
+ 
+  testWidgets('Add and remove a todo', (WidgetTester tester) async {
+    Luassegiempat ls = Luassegiempat();
+    // Build the widget.
+    await tester.pumpWidget(maketestablewidget(child: ls));
+    // Enter 'hi' into the TextField.
+    await tester.tap(find.byKey(Key('luas')));
+
+    expect(find.byKey(Key('luas')), findsOneWidget);
+    expect(find.byKey(Key('keliling')), findsNothing);
+    
+    // Tap the add button.
+   
+
+});
+
+testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Luassegiempat());
 
-    
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-   
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
+
+
 
 }
